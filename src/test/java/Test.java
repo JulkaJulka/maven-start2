2204ed8a107fd50c49b1d3cc0debf0dce7c96a77
Juli <walarm@meta.ua>
2017-10-30 20:34:38 +0200

    maven dependance

---
+++ src/test/java/Test.java
@@ -0,0 +1,10 @@
+import com.google.common.math.IntMath;
+
+/**
+ * Created by user on 30.10.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(IntMath.pow(2, 7));
+    }
+}
